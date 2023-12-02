2ddcbdeb9326badf04ea131478e1644af95c746d
Jeff Manning <info@jeffmanning.ca>
2023-12-02 14:39:29 -0500

    day 1 part 1

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::fs;
+
+fn main() {
+    day_one();
+}
+
+fn day_one() {
+    // let mut contents = Vec::new();
+    let mut total = 0;
+    for line in  fs::read_to_string("day_one.txt").unwrap().lines() {
+        let mut number = 0;
+        //get first char
+        for c in line.chars() {
+            if c.is_numeric() {
+                number += c.to_digit(10).unwrap() * 10;
+                break;
+            }
+        }
+
+        for c in line.chars().rev() {
+            if c.is_numeric() {
+                number += c.to_digit(10).unwrap();
+                break;
+            }
+        }
+        total += number;
+    }
+    print!("{}",total);
+}
